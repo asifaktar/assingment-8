0894920290dac443ee27027a3e93f89c4ed26737
asifaktar <asifaktar7767@gmail.com>
2025-02-20 23:28:25 +0530

    added a some changes

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { reactHooks } from 'eslint-plugin-react-hooks';
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <button className="btn btn-neutral">Neutral</button>
+      </div>
+      <h2 className='text-3xl'>Hi</h2>
+    </> 
+  );
+};
+
+export default App;
---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,14 @@
+
+
+const ErrorPage = () => {
+    return (
+        <div className="justify-items-center mt-10">
+            <div className="rounded-2xl bg-amber-300 text-center p-10 md:p-20 lg:p-30">
+                <h2 className="font-bold text-2xl md:text-4xl ">Page not found</h2>
+                <p className="text-2xl md:text-4xl font-medium">Status: 4o4</p>
+            </div>
+        </div>
+    );
+};
+
+export default ErrorPage;
